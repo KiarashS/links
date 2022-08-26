2c79e296d0dcd99c040dc90fa9bdd6f00df1d984
KiarashS <kiarash.s@hotmail.com>
2022-08-26 09:57:38 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Engineer", "Researcher", "ML/AI Enthusiast", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
