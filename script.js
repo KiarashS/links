8f53ebcd4e2f6a3cdd25d3b1d150ebbf9d369241
Kiarash <1054134+KiarashS@users.noreply.github.com>
2023-07-15 11:00:12 +0330

    UPdate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Engineer", "Researcher", "ML/AI Enthusiast", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Engineer", "Researcher", "ML/AI Enthusiast", "Developer", "UI/UX Designer", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         cursorChar: ' |',
